6c0b1773cc9401199a3e10487252f6c6234d0775
Mathews Paku <mathewspaku@gmail.com>
2023-05-18 14:54:05 +0200

    Added docker file

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname,'..','public')));
 app.use('/planets',planetRouter);
 app.use('/launches',launchesRouter);
 app.get('/*',(req, res)=>{
-    res.sendFile(path.join(__dirname, '..','public','index.html'));
+    res.send('working');
 })
 
 module.exports = app;
